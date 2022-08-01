a33a3cb9c61000ee7e4fce40857c6e2dad738658
CloudHub-NL <110393785+CloudHub-NL@users.noreply.github.com>
2022-08-01 13:59:37 +0200

    Update linear-regression.py

--- linear-regression.py
+++ linear-regression.py
@@ -3,7 +3,7 @@
 
 import matplotlib.pyplot as plt
 from scipy import stats
-
+#define values of x & y
 x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
 y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
@@ -14,6 +14,7 @@ def myfunc(x):
 
 mymodel = list(map(myfunc, x))
 
+#Plot it using pyplot Scatter
 plt.scatter(x, y)
 plt.plot(x, mymodel)
 plt.show()
